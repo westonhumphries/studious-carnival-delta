b299f53c061b8946214c3630a97310e237735a4c
westonhumphries <122556134+westonhumphries@users.noreply.github.com>
2023-02-20 18:59:08 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,128 @@
-$(function () {
-  console.log("show me the money");
-//   $( "#draggable" ).draggable();
+$(document).ready(function () {
+  console.log("let get this puppy started");
 
-var $drag_counter = $("#event-drag"),
-counts = 0;
+  //let revName;
 
+  $("#btnGetFacts").click(function () {
+    $("#coolNameFactsOutput").html();
+    // handle it if input is blank
 
- $("#draggable").draggable({
-  drag: function () {
-    counts++;
-    updateCounterStatus($drag_counter, counts);
+    let userName = $("#userName").val();
+    console.log("user name = ", userName);
+    //let coolFacts = generateCoolNameFacts(userName);
 
-  },
+    console.log("name length f/n = ", nameLength(userName));
 
- });
+    console.log("you clicked the button");
+    //console.log("from ln 12", generateCoolNameFacts(userName));
 
-// took from Dr cumbie
- $("#resetGame").click(function () {
-  console.log("clicked button");
-  counts = 0;
-
-  $("span.count").text(counts);
-  $("#gameOutput").text("");
-
-  $("#draggable").fadeOut(40).animate({ left: 0 }).fadeIn(30);
+    $("#coolNameFactsOutput").html(nameLength(userName));
+    $("#coolNameFactsOutputss").html(getSpiritAnimal(userName));
+  });
 });
 
 
 
- function updateCounterStatus($bravo_counter, drag_count) {
+/*
+function generateCoolNameFacts(userName) {
+  let coolFacts = "";
 
-//todo: if more than 174, then wow. if not, no wow
-    
-    var game_msg = "";
+  coolFacts = nameLength(userName);
+  coolFacts += " <br> " + nameStart(userName);
+  coolFacts += "<h3> " + nameEnd(userName) + "</h3>";
+  coolFacts += "<h2>" + reverseName(userName) + "</h2>";
 
-    if (drag_count < 350) {
-      game_msg = "Drag longer to win";
-    } else if (drag_count == 1200) {
-      game_msg = "wow can't believe you found this, Well you are the actual WINNER, WOW";
-    } else {
-      game_msg = "Nice job you wasted your time just for this win, WOW";
-      $("img").attr(
-        "src",
-        "https://i.imgflip.com/478jfr.png"
-      );
-    }
+  console.log("initialized the'm coolFacts variable: " + coolFacts);
 
-    $("#gameOutput").text(game_msg);
-    $("span.count", $bravo_counter).text(drag_count);
+  return coolFacts;
+}
+*/
 
 
+function nameLength(userName) {
+  //named, not anonymous. Name = nameLength
+  //parameters, one = j
+  //void if it has no return value
 
+  return "Your name is " + userName.length + " letters long!";
+}
+
+
+/*
+function nameStart(name) {
+  return "the first letter of your name is " + name[0];
+}
+
+function nameEnd(name) {
+  let lastIndex = name.length - 1;
+
+  return "the last letter of your name is " + name[lastIndex];
+}
+*/
+/*
+function reverseName(name) {
+  // let arrSomeArray = [1, "cat", "Susan", [j, "stuff"]];
+
+  let arrName = name.split();
+
+  revName = arrName.reverse();
+}
+*/
+function getSpiritAnimal(userName) {
+  //flow control: case switch, loop, if/then
+
+  if (userName.length == 1) {
+    //execute some code
+    return "your spirit animal is a Snake";
+  } 
+
+  else if (userName.length == 2) {
+    //execute some code
+    return "your spirit animal is a Fox";
+  } 
+
+  else if (userName.length == 3) {
+    //execute some code
+    return "your spirit animal is a Liger";
+  } 
+
+  else if (userName.length == 4) {
+    //execute some code
+    return "your spirit animal is a Dragon";
+  } 
+
+  else if (userName.length == 5) {
+    //execute some code
+    return "your spirit animal is a Lion";
+  } 
+
+  else if (userName.length == 6) {
+    //execute some code
+    return "your spirit animal is a Niffler";
+  } 
+
+  else if (userName.length == 7) {
+    //execute some code
+    return "your spirit animal is a Owl";
+  } 
+
+  else if (userName.length == 8) {
+    //execute some code
+    return "your spirit animal is a Otter";
+  } 
+
+  else if (userName.length == 9) {
+    //execute some code
+    return "your spirit animal is a Shark";
+  } 
+
+  else if (userName.length > 9) {
+    //execute some code
+    return "yo first name is kinda long, your spirit animal is a Giraffe!!!!";
+  } 
+
+  else {
+    return "You don't play my game, you get no name!!!!!";
   }
-
-});
+  
+}
